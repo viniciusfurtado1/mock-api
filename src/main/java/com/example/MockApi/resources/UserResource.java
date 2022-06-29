9f6d8cf9fabbbabe3bab6e8f217d7c8b4f096e1a
viniciusfurtado1 <vinicius_furtado10@hotmail.com>
2022-06-29 19:03:33 -0300

    classe user resource findById

--- src/main/java/com/example/MockApi/resources/UserResource.java
+++ src/main/java/com/example/MockApi/resources/UserResource.java
@@ -1,6 +1,8 @@
 package com.example.MockApi.resources;
 
 import com.example.MockApi.domain.User;
+import com.example.MockApi.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping("/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id){
-
-        return ResponseEntity.ok().body(new User(1, "Valdir", "val@mail.com", "123"));
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
