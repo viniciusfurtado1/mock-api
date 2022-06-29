30f479d1340f8bcbc8641d7ca8d62017dadaf918
viniciusfurtado1 <vinicius_furtado10@hotmail.com>
2022-06-29 19:02:09 -0300

    interface user repository

---
+++ src/main/java/com/example/MockApi/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.MockApi.repositories;
+
+import com.example.MockApi.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
