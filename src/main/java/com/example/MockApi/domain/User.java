31faa6d517eff202d47470245faeca60b25a70e3
viniciusfurtado1 <vinicius_furtado10@hotmail.com>
2022-06-29 18:19:30 -0300

    classe user

---
+++ src/main/java/com/example/MockApi/domain/User.java
@@ -0,0 +1,21 @@
+package com.example.MockApi.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
